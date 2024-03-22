29b8613add154d67a2c51ed248efc4bd9e3e93f2
jaeyeoneej <5ikve@naver.com>
2024-03-22 09:47:28 +0900

    check mediaDevices

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Home = lazy(() => import(/* webpackChunkName: "home" */ "./routes/Home"));
 const Scan = lazy(() => import(/* webpackChunkName: "scan" */ "./routes/Scan"));
 
 function App() {
+  useEffect(() => {
+    navigator?.mediaDevices
+      ?.getUserMedia({ video: true, audio: true })
+      .then(function (stream) {
+        console.log("jjy 카메라 및 마이크 엑세스가 허용되었습니다.");
+      })
+      .catch(function (err) {
+        console.log(
+          "jjy 카메라 및 마이크 액세스를 거부하거나 오류가 발생했습니다: ",
+          err
+        );
+      });
+  }, []);
+
   return (
     <Router>
       <Suspense fallback={<div>loading...</div>}>
